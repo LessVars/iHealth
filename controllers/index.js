b92f2a2b7b1e99b872f5e7d6bffac1a6d1d64020
hexcola <hexcola@gmail.com>
2017-06-29 18:33:55 +0800

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
     , router = express.Router()
+    , auth    = require('../middleware/auth.js').auth
 
 router.get('/', function(req, res, next){
     res.render('index.html', {greeting: 'Hello'})
 })
 
+/**
+ * if we have mutliple page, we should keep some reserve words for
+ * navigation, e.g. home, about, service, product, etc.
+ */
+router.get('/:username', auth, function(req, res, next){
+    username = req.params.username
+    res.render('profile.html', { username: username })
+})
 
 /**
  * we can use glob to regist in one shoot!
--- controllers/medical_record.js
+++ controllers/medical_record.js
@@ -1,10 +1,28 @@
 const express = require('express')
     , router = express.Router()
 
+router.get('/:username/medical_record', function(req, res, next){
+    username = req.params.username
+
+    if (username == "neo")
+    {
+        res.send('awesome!')
+    }
+    else 
+    {
+        res.send('sucks.')
+    }
+})
 
 /**
  * we can use glob to regist in one shoot!
  */
+// module.exports = function(app){
+//     // all request must authenticated.
+//     app.use('/medical_record', auth, router)
+// }
+
 module.exports = function(app){
-    app.use('/medical_record', router)
+    // all request must authenticated.
+    app.use('/', router)
 }
